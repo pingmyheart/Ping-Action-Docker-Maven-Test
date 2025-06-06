fe6e946b480205e9530b8f609f0a619125f93f5d
pingmyheart <antoniorussi1972@gmail.com>
2025-06-06 17:16:05 +0200

    [UPDATE] - CI

---
+++ src/test/java/io/github/pingmyheart/pingactiondockermaventest/ApplicationTest.java
@@ -0,0 +1,20 @@
+package io.github.pingmyheart.pingactiondockermaventest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
